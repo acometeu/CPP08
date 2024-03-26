e2db18eaffbfa149a7600bb2ff1db99d1e83d064
leacomet <alexandrecomet1@gmail.com>
2024-03-26 17:03:47 +0100

    finish

--- ex02/MutantStack.hpp
+++ ex02/MutantStack.hpp
@@ -14,6 +14,7 @@
 #define MUTANTSTACK_HPP
 
 #include <iostream>
+#include <algorithm>
 #include <stack>
 #include <iterator>
 
@@ -23,18 +24,80 @@ class	MutantStack : public std::stack<T>
 
 public:
 
-	MutantStack(void);
-	MutantStack(unsigned int N);
-	MutantStack(MutantStack const &src);
-	~MutantStack(void);
-	MutantStack	&operator=(MutantStack const &src);
+	typedef typename std::stack<T>::container_type::iterator				iterator;
+	typedef typename std::stack<T>::container_type::reverse_iterator		reverse_iterator;
+	typedef typename std::stack<T>::container_type::const_iterator			const_iterator;
+	typedef typename std::stack<T>::container_type::const_reverse_iterator	const_reverse_iterator;
 
+	MutantStack(void) : std::stack<T>() {}
+	MutantStack(const MutantStack<T> &src) : std::stack<T>() {
+		*this = src;
+	} 
+	~MutantStack(void) {}
+	MutantStack<T>	&operator=(MutantStack<T> const &src) {
+		std::stack<T>::operator=(src);
+		return *this;
+	}
 
-
-private:
-
-	typedef std::stack<T>::forward_iterator_tag iterator;
+	iterator				begin(void);
+	iterator				end(void);
+	reverse_iterator		rbegin(void);
+	reverse_iterator		rend(void);
+	const_iterator			cbegin(void);
+	const_iterator			cend(void);
+	const_reverse_iterator	crbegin(void);
+	const_reverse_iterator	crend(void);
 
 };
+
+template<typename T>
+typename MutantStack<T>::iterator	MutantStack<T>::begin(void)
+{
+	return (std::stack<T>::c.begin());
+}
+
+template<typename T>
+typename MutantStack<T>::iterator	MutantStack<T>::end(void)
+{
+	return (std::stack<T>::c.end());
+}
+
+template<typename T>
+typename MutantStack<T>::reverse_iterator	MutantStack<T>::rbegin(void)
+{
+	return (std::stack<T>::c.rbegin());
+}
+
+template<typename T>
+typename MutantStack<T>::reverse_iterator	MutantStack<T>::rend(void)
+{
+	return (std::stack<T>::c.rend());
+}
+
+template<typename T>
+typename MutantStack<T>::const_iterator	MutantStack<T>::cbegin(void)
+{
+	return (std::stack<T>::c.cbegin());
+}
+
+template<typename T>
+typename MutantStack<T>::const_iterator	MutantStack<T>::cend(void)
+{
+	return (std::stack<T>::c.cend());
+}
+
+template<typename T>
+typename MutantStack<T>::const_reverse_iterator	MutantStack<T>::crbegin(void)
+{
+	return (std::stack<T>::c.crbegin());
+}
+
+template<typename T>
+typename MutantStack<T>::const_reverse_iterator	MutantStack<T>::crend(void)
+{
+	return (std::stack<T>::c.crend());
+}
+
+
 
 #endif
--- ex02/main.cpp
+++ ex02/main.cpp
@@ -1,155 +1,202 @@
-#include "Span.hpp"
+#include "MutantStack.hpp"
 #include <array>
 #include <vector>
+#include <list>
 
 int main(void)
 {
 {
-    /*  test du test  */
-    Span sp = Span(5);
+    /*TEST SUBJECT */
+    MutantStack<int> mstack;
 
-    sp.addNumber(6);
-    sp.addNumber(3);
-    sp.addNumber(17);
-    sp.addNumber(9);
-    sp.addNumber(11);
-    std::cout << "sp[0] = " << sp[0] << std::endl;
-    std::cout << "sp[1] = " << sp[1] << std::endl;
-    std::cout << "sp[2] = " << sp[2] << std::endl;
-    std::cout << "sp[3] = " << sp[3] << std::endl;
-    std::cout << "sp[4] = " << sp[4] << std::endl;
-    std::cout << sp.shortestSpan() << std::endl;
-    std::cout << sp.longestSpan() << std::endl;
+    mstack.push(5);
+    mstack.push(17);
+
+    std::cout << mstack.top() << std::endl;
+
+    mstack.pop();
+
+    std::cout << mstack.size() << std::endl;
+
+    mstack.push(3);
+    mstack.push(5);
+    mstack.push(737);
+    //[...]
+    mstack.push(0);
+
+    MutantStack<int>::iterator it = mstack.begin();
+    MutantStack<int>::iterator ite = mstack.end();
+
+    ++it;
+    --it;
+    while (it != ite)
     {
-        /*  test copy and operator = */
-        Span    copy(sp);
-        Span    equal = sp;
-        std::cout << "copy test" << std::endl;
-        std::cout << "copy[0] = " << copy[0] << std::endl;
-        std::cout << "copy[1] = " << copy[1] << std::endl;
-        std::cout << "copy[2] = " << copy[2] << std::endl;
-        std::cout << "copy[3] = " << copy[3] << std::endl;
-        std::cout << "copy[4] = " << copy[4] << std::endl;
-        std::cout << std::endl;
-        std::cout << "equal test" << std::endl;
-        std::cout << "equal[0] = " << equal[0] << std::endl;
-        std::cout << "equal[1] = " << equal[1] << std::endl;
-        std::cout << "equal[2] = " << equal[2] << std::endl;
-        std::cout << "equal[3] = " << equal[3] << std::endl;
-        std::cout << "equal[4] = " << equal[4] << std::endl;
+        std::cout << *it << std::endl;
+        ++it;
     }
+    std::stack<int> s(mstack);
 }
+
 std::cout << std::endl << std::endl;
 {
-    /*  petit test  */
-    Span    test;
+    /*TEST SUBJECT to compare with list */
+    std::list<int>  mstack;
 
-    std::cout << "test max is : " << test.get_max() <<  std::endl;
-    std::cout << "test number is : " << test.get_number() <<  std::endl;
-    try
-    {
-        std::cout << "test[0] = " << test[0] <<  std::endl;
-    }
-    catch (std::exception &e)
-    {
-        std::cerr << e.what() << std::endl;
-    }
-    test.addNumber(20);
-    test.addNumber(30);
-    test.addNumber(40);
-    test.addNumber(-50);
-    test.addNumber(-60);
-    test.addNumber(70);
-    test.addNumber(-80);
-    test.addNumber(90);
-    test.addNumber(32);
-    test.addNumber(35);
-    try
-    {
-        int i = 0;
+    mstack.push_back(5);
+    mstack.push_back(17);
 
-        while (i < 10)
-        {
-            std::cout << "test[" << i << "] = " << test[i] <<  std::endl;
-            i++;
-        }
-    }
-    catch (std::exception &e)
+    std::cout << mstack.back() << std::endl;
+
+    mstack.pop_back();
+
+    std::cout << mstack.size() << std::endl;
+
+    mstack.push_back(3);
+    mstack.push_back(5);
+    mstack.push_back(737);
+    //[...]
+    mstack.push_back(0);
+
+    std::list<int>::iterator it = mstack.begin();
+    std::list<int>::iterator ite = mstack.end();
+
+    ++it;
+    --it;
+    while (it != ite)
     {
-        std::cerr << e.what() << std::endl;
+        std::cout << *it << std::endl;
+        ++it;
     }
-    try
-    {
-        std::cout << "shortest_span = " << test.shortestSpan() <<  std::endl;
-        std::cout << "longest_span = " << test.longestSpan() <<  std::endl;
-    }
-    catch (std::exception &e)
-    {
-        std::cerr << e.what() << std::endl;
-    }
+    std::list<int> s(mstack);
 }
 std::cout << std::endl << std::endl;
 
-{
-    /* 10001 numbers */
-    Span    test(10001);
-    int i = 0;
-
-    while (i < 6000)
-    {
-        test.addNumber(i * 999 + 7);
-        i++;
-    }
-    test.addNumber(3599 *999 + 677);
-    while (i < 10000)
-    {
-        test.addNumber(i * 999 + 7);
-        i++;
-    }
-    std::cout << "boucle 10000 longest Span = " << test.longestSpan() << std::endl;
-    std::cout << "boucle 10000 shortest Span = " << test.shortestSpan() << std::endl;
-}
-std::cout << std::endl << std::endl;
 
 {
-    /* TUH-TUH-TES-TUH  */
-    std::array<int, 5>  array = {-55555, 6236345, 49218743, -2147483648, 2147483647};
-    std::vector<int>    biguh(10000, 7); 
-    Span                for_harray(5);
-    Span                for_biguh(10001);
+    MutantStack<int> test;
+    MutantStack<int>::iterator   it;
 
-    try
-    {
-        for_harray.addManyNumber(array);
-        std::cout << "array[0] = " << for_harray[0] << std::endl;
-        std::cout << "array[1] = " << for_harray[1] << std::endl;
-        std::cout << "array[2] = " << for_harray[2] << std::endl;
-        std::cout << "array[3] = " << for_harray[3] << std::endl;
-        std::cout << "array[4] = " << for_harray[4] << std::endl;
-        std::cout << "longest span for Harray : " << for_harray.longestSpan() << std::endl;
-        std::cout << "shortest span for Harray : " << for_harray.shortestSpan() << std::endl;
-    }
-    catch(const std::exception& e)
-    {
-        std::cerr << e.what() << std::endl;
-    }
-    try
-    {
-        for_biguh.addManyNumber(biguh);
-        for_biguh.addNumber(999999999);
-        std::cout << "longest span for biguh : " << for_biguh.longestSpan() << std::endl;
-        std::cout << "shortest span for biguh : " << for_biguh.shortestSpan() << std::endl;
-        /*  random valid number   */
-        std::cout << "random valid number 1 : " << for_biguh[5555] << std::endl;
-        std::cout << "random valid number 2 : " << for_biguh[55] << std::endl;
-        // std::cout << "random valid number 3 : " << for_biguh[55555] << std::endl;
-        std::cout << "random valid number 4 : " << for_biguh[10000] << std::endl;
-    }
-    catch(const std::exception& e)
-    {
-        std::cerr << e.what() << std::endl;
-    }
+    std::cout << "test with INT" << std::endl;
+    test.push(7);
+    std::cout << "top of stack test = " << test.top() << std::endl;
+    test.push(999);
+    std::cout << "top of stack test = " << test.top() << std::endl;
+
+    it = test.begin();
+    std::cout << "iterator it from beningin = " << *it << std::endl;
+    it++;
+    std::cout << "iterator it from beningin = " << *it << std::endl;
+    it --;
+    std::cout << "iterator it from beningin = " << *it << std::endl;
+
+    it = test.end();
+    it--;
+    std::cout << "iterator it from end = " << *it << std::endl << std::endl;
     
+    {
+        /*  test copy   */   
+        MutantStack<int> copy(test);
+        MutantStack<int> equal = test;
+        MutantStack<int>::iterator   it;
+
+        it = copy.begin();
+        std::cout << "copy values :" << std::endl;
+        std::cout << *it << std::endl;
+        it++;
+        std::cout << *it << std::endl << std::endl;
+
+        it = equal.begin(); 
+        std::cout << "equal values :" << std::endl;
+        std::cout << *it << std::endl;
+        it++;
+        std::cout << *it << std::endl << std::endl;
+    }
+}
+
+{
+    MutantStack<std::string> test;
+    MutantStack<std::string>::iterator   it;
+
+    std::cout << "test with STRING" << std::endl;
+    test.push("et vas-y");
+    std::cout << "top of stack test = " << test.top() << std::endl;
+    test.push("ouioui !");
+    std::cout << "top of stack test = " << test.top() << std::endl;
+
+    it = test.begin();
+    std::cout << "iterator it from beningin = " << *it << std::endl;
+    it++;
+    std::cout << "iterator it from beningin = " << *it << std::endl;
+    it --;
+    std::cout << "iterator it from beningin = " << *it << std::endl;
+
+    it = test.end();
+    it--;
+    std::cout << "iterator it from end = " << *it << std::endl << std::endl;
+    
+    {
+        /*  test copy   */   
+        MutantStack<std::string> copy(test);
+        MutantStack<std::string> equal = test;
+        MutantStack<std::string>::iterator   it;
+
+        it = copy.begin();
+        std::cout << "copy values :" << std::endl;
+        std::cout << *it << std::endl;
+        it++;
+        std::cout << *it << std::endl << std::endl;
+
+        it = equal.begin(); 
+        std::cout << "equal values :" << std::endl;
+        std::cout << *it << std::endl;
+        it++;
+        std::cout << *it << std::endl << std::endl;
+    }
+}
+{
+    /*  more iterator type  */
+    MutantStack<long int> more;
+
+    MutantStack<long int>::reverse_iterator reverse_it;
+    MutantStack<long int>::reverse_iterator reverse_it2;
+    MutantStack<long int>::const_iterator const_it;
+    MutantStack<long int>::const_iterator const_it2;
+    MutantStack<long int>::const_reverse_iterator const_reverse_it;
+    MutantStack<long int>::const_reverse_iterator const_reverse_it2;
+
+    more.push(1);
+    more.push(2);
+    more.push(3);
+    more.push(4);
+    more.push(5);
+    more.push(6);
+    more.push(7);
+
+    reverse_it = more.rbegin();
+    reverse_it2 = more.rend();
+    std::cout << "boucle reverse" << std::endl;
+    while (reverse_it != reverse_it2)
+    {
+        std::cout << "reverse_it = " << *reverse_it << std::endl;
+        reverse_it++;
+    }
+
+    const_it = more.cbegin();
+    const_it2 = more.cend();
+    while (const_it != const_it2)
+    {
+        std::cout << "const_it = " << *const_it << std::endl;
+        const_it++;
+    }
+
+    const_reverse_it = more.crbegin();
+    const_reverse_it2 = more.crend();
+    while (const_reverse_it != const_reverse_it2)
+    {
+        std::cout << "const_reverse_it = " << *const_reverse_it << std::endl;
+        const_reverse_it++;
+    }
+
 
 }
     return(0);
